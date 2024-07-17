22419a09d492fc16b3f495ac650312f9e11d5ac1
wakabame <wakabame89@gmail.com>
2024-07-17 06:42:27 +0000

    Apply format

--- source/conf.py
+++ source/conf.py
@@ -6,23 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'python_research_software_templete'
-copyright = '2024, wakabame'
-author = 'wakabame'
+project = "python_research_software_templete"
+copyright = "2024, wakabame"
+author = "wakabame"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-root_doc = 'index'
+root_doc = "index"
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
