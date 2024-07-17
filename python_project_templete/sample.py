27e6b0205cdf3dd70db4d00c9d23f96ae0c981f5
Yuki Kimura <35513518+wakabame@users.noreply.github.com>
2024-07-17 14:52:09 +0900

    Initial commit

---
+++ python_project_templete/sample.py
@@ -0,0 +1,2 @@
+def sample_add_int(x, y) -> int:
+    return x + y
---
+++ tests/test_sample.py
@@ -0,0 +1,5 @@
+from python_project_templete.sample import sample_add_int
+
+
+def test_sample_add_int() -> None:
+    assert sample_add_int(1, 2) == 3
