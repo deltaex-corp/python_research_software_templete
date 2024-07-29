318bfadfbcb19298956dc8b84e41c7345c15d322
wakabame <wakabame89@gmail.com>
2024-07-29 14:51:09 +0000

    numpy style で docstrings を追加

--- python_research_software_templete/sample.py
+++ python_research_software_templete/sample.py
@@ -1,2 +1,11 @@
-def sample_add_int(x, y) -> int:
+def sample_add_int(x: int, y: int) -> int:
+    """２つの整数の和を返します
+
+    Args:
+        x (int): 整数そのいち
+        y (int): 整数そのに
+
+    Returns:
+        int: ２つの値の合計
+    """
     return x + y
