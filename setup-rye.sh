fdb1c04f82367a9195793d00354ae26f222087e0
wakabame <wakabame89@gmail.com>
2024-10-31 12:11:29 +0000

    インストールオプションをワンライナーで

--- setup-rye.sh
+++ setup-rye.sh
@@ -1,6 +1,4 @@
 #/bin/bash
-# Ryeのバージョン固定
-RYE_VERSION=0.42.0
-curl -sSf https://rye.astral.sh/get | RYE_INSTALL_OPTION="--yes" bash
+curl -sSf https://rye.astral.sh/get | RYE_VERSION=0.42.0 RYE_INSTALL_OPTION="--yes" bash
 source "$HOME/.rye/env"
 rye config --set-bool behavior.use-uv=true
